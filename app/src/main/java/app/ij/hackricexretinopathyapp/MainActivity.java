856bc2e629c520ae10aa9aab3a46a57c85d725cc
ishaanjav <ishaanjav@gmail.com>
2020-09-19 11:27:39 -0500

    More progress

--- app/src/main/java/app/ij/hackricexretinopathyapp/MainActivity.java
+++ app/src/main/java/app/ij/hackricexretinopathyapp/MainActivity.java
@@ -1,14 +1,29 @@
 package app.ij.hackricexretinopathyapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button camera, gallery;
+    ContentValues values;
+    Uri imageUri;
+
+    int PICTURE_RESULT = 1, GALLERY_RESULT = 2;
+    Bitmap img;
+    String imageurl;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void buttons(){
+    void buttons() {
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                values = new ContentValues();
+                imageUri = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, PICTURE_RESULT);
             }
         });
         gallery.setOnClickListener(new View.OnClickListener() {
@@ -38,9 +58,50 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public Bitmap centerCrop(Bitmap srcBmp) {
+        if (srcBmp.getWidth() >= srcBmp.getHeight())
+            return Bitmap.createBitmap(srcBmp, srcBmp.getWidth() / 2 - srcBmp.getHeight() / 2, 0, srcBmp.getHeight(), srcBmp.getHeight());
+        return Bitmap.createBitmap(srcBmp, 0, srcBmp.getHeight() / 2 - srcBmp.getWidth() / 2, srcBmp.getWidth(), srcBmp.getWidth());
+    }
+
+    // Get the result from the Camera/Gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // They didn't cancel
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICTURE_RESULT) {
+                try {
+                    img = MediaStore.Images.Media.getBitmap(
+                            getContentResolver(), imageUri);
+                    img = centerCrop(img);
+                    imageView.setImageBitmap(img);
+                    imageurl = getRealPathFromURI(imageUri);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }else{
+                // TODO
+                //  Get image from gallery here.
+
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
     void bindViews() {
         camera = findViewById(R.id.camera);
         gallery = findViewById(R.id.gallery);
+        imageView = findViewById(R.id.imageView);
     }
 
 }
