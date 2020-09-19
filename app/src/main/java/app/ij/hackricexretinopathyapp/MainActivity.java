a330d9c6c9789fa3a30f1b8a7c9136ace55a4760
ishaanjav <ishaanjav@gmail.com>
2020-09-19 12:45:27 -0500

    Getting images complete

--- app/src/main/java/app/ij/hackricexretinopathyapp/MainActivity.java
+++ app/src/main/java/app/ij/hackricexretinopathyapp/MainActivity.java
@@ -8,17 +8,27 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     Bitmap img;
     String imageurl;
     ImageView imageView;
-    int REQUEST_CAMERA = 1034;
+    int REQUEST_CAMERA = 1034, REQUEST_GALLERY = 1000, REQUEST_GALLERY_FROM_CAMERA = 234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +70,26 @@ public class MainActivity extends AppCompatActivity {
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_DENIED) {
+                    //permission not granted request it.
+                    String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    //show popupƒ
+                    requestPermissions(permissions, REQUEST_GALLERY);
+                } else {
+                    /* permission already granted */
+                    pickImageFromGallery();
+                }
             }
         });
     }
 
-    public Bitmap centerCrop(Bitmap srcBmp) {
-        if (srcBmp.getWidth() >= srcBmp.getHeight())
-            return Bitmap.createBitmap(srcBmp, srcBmp.getWidth() / 2 - srcBmp.getHeight() / 2, 0, srcBmp.getHeight(), srcBmp.getHeight());
-        return Bitmap.createBitmap(srcBmp, 0, srcBmp.getHeight() / 2 - srcBmp.getWidth() / 2, srcBmp.getWidth(), srcBmp.getWidth());
+    private void pickImageFromGallery() {
+        //intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_RESULT);
+
     }
 
     // Get the result from the Camera/Gallery
@@ -82,27 +103,47 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     img = MediaStore.Images.Media.getBitmap(
                             getContentResolver(), imageUri);
+                    img = handleSamplingAndRotationBitmap(getApplicationContext(), imageUri);
                     img = centerCrop(img);
                     imageView.setImageBitmap(img);
                     imageurl = getRealPathFromURI(imageUri);
+                    makeToast("Got the image");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             } else {
-                // TODO
-                //  Get image from gallery here.
-
+                imageUri = data.getData();
+                try {
+                    img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    img = handleSamplingAndRotationBitmap(getApplicationContext(), imageUri);
+                    img = centerCrop(img);
+                    imageView.setImageBitmap(img);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    makeToast("Error getting image");
+                    Log.wtf("*Gallery error: ", e.toString());
+                }
             }
         }
     }
 
     void launchCamera() {
-        values = new ContentValues();
-        imageUri = getContentResolver().insert(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        startActivityForResult(intent, PICTURE_RESULT);
+        try {
+            values = new ContentValues();
+            imageUri = getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, PICTURE_RESULT);
+        } catch (Exception e) {
+            Log.wtf("*Error launching Camera", e.toString());
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_GALLERY_FROM_CAMERA);
+            } else {
+                pickImageFromGallery();
+            }
+        }
     }
 
     @Override
@@ -113,9 +154,102 @@ public class MainActivity extends AppCompatActivity {
                     == PackageManager.PERMISSION_GRANTED) {
                 launchCamera();
             }
+        } else if (requestCode == REQUEST_GALLERY) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                pickImageFromGallery();
+            }
+        } else if (requestCode == REQUEST_GALLERY_FROM_CAMERA) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                launchCamera();
+            }
         }
 
     }
+
+    void bindViews() {
+        camera = findViewById(R.id.camera);
+        gallery = findViewById(R.id.gallery);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public Bitmap centerCrop(Bitmap srcBmp) {
+        if (srcBmp.getWidth() >= srcBmp.getHeight())
+            return Bitmap.createBitmap(srcBmp, srcBmp.getWidth() / 2 - srcBmp.getHeight() / 2, 0, srcBmp.getHeight(), srcBmp.getHeight());
+        return Bitmap.createBitmap(srcBmp, 0, srcBmp.getHeight() / 2 - srcBmp.getWidth() / 2, srcBmp.getWidth(), srcBmp.getWidth());
+    }
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
+    }
+
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+        InputStream input = context.getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+    public static Bitmap handleSamplingAndRotationBitmap(Context context, Uri selectedImage) throws IOException {
+        int MAX_HEIGHT = 1024;
+        int MAX_WIDTH = 1024;
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        InputStream imageStream = context.getContentResolver().openInputStream(selectedImage);
+        BitmapFactory.decodeStream(imageStream, null, options);
+        imageStream.close();
+
+        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT);
+
+        options.inJustDecodeBounds = false;
+        imageStream = context.getContentResolver().openInputStream(selectedImage);
+        Bitmap img = BitmapFactory.decodeStream(imageStream, null, options);
+
+        img = rotateImageIfRequired(context, img, selectedImage);
+        return img;
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+            final float totalPixels = width * height;
+            final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+                inSampleSize++;
+            }
+        }
+        return inSampleSize;
+    }
+
 
     public String getRealPathFromURI(Uri contentUri) {
         String[] proj = {MediaStore.Images.Media.DATA};
@@ -126,10 +260,12 @@ public class MainActivity extends AppCompatActivity {
         return cursor.getString(column_index);
     }
 
-    void bindViews() {
-        camera = findViewById(R.id.camera);
-        gallery = findViewById(R.id.gallery);
-        imageView = findViewById(R.id.imageView);
-    }
 
+    Toast t;
+
+    public void makeToast(String s) {
+        if (t != null) t.cancel();
+        t = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
+        t.show();
+    }
 }
