c7d800b5cb0e403d594f9403b351956128535256
ishaanjav <ishaanjav@gmail.com>
2020-09-19 11:09:07 -0500

    Camera buttons

--- app/src/main/java/app/ij/hackricexretinopathyapp/MainActivity.java
+++ app/src/main/java/app/ij/hackricexretinopathyapp/MainActivity.java
@@ -3,13 +3,44 @@ package app.ij.hackricexretinopathyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button camera, gallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Function to bind XML views to Java Objects
+        bindViews();
+
+        buttons();
+
+
+    }
+
+    void buttons(){
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    void bindViews() {
+        camera = findViewById(R.id.camera);
+        gallery = findViewById(R.id.gallery);
     }
 
 }
