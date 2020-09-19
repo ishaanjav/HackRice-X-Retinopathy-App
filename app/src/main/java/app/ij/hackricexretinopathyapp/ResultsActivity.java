6e69b178eb0b891a683df67ed2b3110d22388221
ishaanjav <ishaanjav@gmail.com>
2020-09-19 14:47:41 -0500

    Additional changes before opencv

--- app/src/main/java/app/ij/hackricexretinopathyapp/ResultsActivity.java
+++ app/src/main/java/app/ij/hackricexretinopathyapp/ResultsActivity.java
@@ -3,11 +3,13 @@ package app.ij.hackricexretinopathyapp;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
@@ -20,7 +22,7 @@ import android.widget.Toast;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,9 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         Button startBtn = (Button) findViewById(R.id.sendEmail);
+        title = findViewById(R.id.title);
+        title.setPaintFlags(title.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
         startBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 sendEmail();
@@ -35,14 +40,13 @@ public class ResultsActivity extends AppCompatActivity {
         });
     }
 
-    public void sendEmail()
-    {
+    public void sendEmail() {
         Log.i("Send email", "");
         String[] TO = {""};
         String[] CC = {""};
 
         Intent emailIntent = new Intent(Intent.ACTION_VIEW);
-        Uri data = Uri.parse("mailto:?subject=" + "Eye Scan"+ "&body=" + "Hey, my results on Retino" +
+        Uri data = Uri.parse("mailto:?subject=" + "Eye Scan" + "&body=" + "Hey, my results on Retino" +
                 "Scanner are shown below. Please check it out!" + "&to=" + "");
         emailIntent.setData(data);
         startActivity(Intent.createChooser(emailIntent, "Send mail..."));
